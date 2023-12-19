e135c3b22472e40637a7e83e632619983e5b6d9f
Strassburger <strassjm@rose-hulman.edu>
2023-12-19 10:22:19 -0500

    connectivity with pi

--- TestConnection.py
+++ TestConnection.py
@@ -1,15 +1,14 @@
 import socket
-msgFromClient = 'Howdy Server, from your Client'
-bytesToSend = msgFromClient.encode('utf-8')
-serverAddress = ('192.168.43.68', 2222)
-bufferSize = 1024
-UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-while True:
-    cmd = input('What do you want to do with counter')
-    cmd = cmd.encode('utf-8')
-    UDPClient.sendto(cmd, serverAddress)
-    data, address = UDPClient.recvfrom(bufferSize)
-    data = data.decode('utf-8')
-    print('Data from Server', data)
-    print('Server IP Address:', address[0])
-    print('Server Port: ', address[1])
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.43.68', 2222))
+
+file = open('cat_testimage.jpg', 'rb')
+image_data = file.read(2048)
+
+while image_data:
+    client.send(image_data)
+    image_data = file.read(2048)
+
+file.close()
+client.close()
