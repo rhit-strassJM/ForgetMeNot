d9371a953824a63fb42b6e0e7adef40727b5cfb5
Strassburger <strassjm@rose-hulman.edu>
2024-03-05 15:57:17 -0500

    wayyyy cuter login screen

--- GUI/LoginScreen.py
+++ GUI/LoginScreen.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.image import AsyncImage
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -10,7 +11,7 @@ Builder.load_string('''
 <LoginScreen>:
     canvas.before:
         Color:
-            rgba: 0.5, 0.5, 1, 1
+            rgb: 0.820, 0.925, 1
         Rectangle:
             pos: self.pos
             size: self.size
@@ -21,17 +22,22 @@ class LoginScreen(Screen):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
 
-        layout = BoxLayout(orientation='vertical', padding=10)
+        layout = BoxLayout(orientation='vertical', padding=50, spacing=50)
 
-        self.username_input = TextInput(multiline=False)
-        self.password_input = TextInput(password=True, multiline=False)
-        login_button = Button(text='Login', on_press=self.check_credentials)
+        self.username_input = TextInput(hint_text='Enter Username', multiline=False, size_hint_y=None, height=50)
+        self.password_input = TextInput(hint_text='Enter Password', password=True, multiline=False, size_hint_y=None, height=50)
+        login_button = Button(text='Login', on_press=self.check_credentials, background_color=[0.7, 1, 1, 1])
 
-        layout.add_widget(Label(text='Please Enter Username:'))
+        image = AsyncImage(source='DisplayImages/Logo.png', size_hint_y=None, height=300)
+        layout.add_widget(image)
+
+        #layout.add_widget(Label(text='Please Enter Username:'))
         layout.add_widget(self.username_input)
-        layout.add_widget(Label(text='Please Enter Password:'))
+        #layout.add_widget(Label(text='Please Enter Password:'))
         layout.add_widget(self.password_input)
         layout.add_widget(login_button)
+
+
 
         self.add_widget(layout)
 
